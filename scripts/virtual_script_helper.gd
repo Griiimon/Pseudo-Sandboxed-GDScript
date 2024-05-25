7f14f762915605d4dec8ae44f101818ab9bbe862
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-05-26 01:56:46 +0200

    Update virtual_script_helper.gd

--- scripts/virtual_script_helper.gd
+++ scripts/virtual_script_helper.gd
@@ -1,4 +1,5 @@
 class_name VirtualScriptHelper
 
 static var built_in_functions:= {
-	"print": VirtualScript.Function.new("print").set_callable(func(arr: Array): print(arr.map(func(x): return str(x)))) }
+	"print": VirtualScript.Function.new("print").set_callable(func(arr: Array): print("".join(arr.map(func(x): return str(x))))), 
+	"prints": VirtualScript.Function.new("prints").set_callable(func(arr: Array): print(" ".join(arr.map(func(x): return str(x))))) }
