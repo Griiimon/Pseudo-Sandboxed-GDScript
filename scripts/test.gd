d57eb9ae1b9336693c2effa7cf06be17899a25fa
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-06-05 07:12:13 +0200

    Update test.gd

--- scripts/test.gd
+++ scripts/test.gd
@@ -20,9 +20,10 @@ func start():
 	
 	#test1(script)
 	#test2(script)
-	script.parse_file("res://virtual scripts/if_else.txt")
+	#script.parse_file("res://virtual scripts/if_else.txt")
 	#script.parse_file("res://virtual scripts/built_in_funcs.txt")
 	#script.parse_file("res://virtual scripts/loops.txt")
+	script.parse_file("res://virtual scripts/function.txt")
 	print("---")
 	script.dump_node_tree(true)
 	print("---")
@@ -32,11 +33,12 @@ func start():
 	
 	script.dump_variables()
 	
-	get_tree().quit.call_deferred()
+	#get_tree().quit.call_deferred()
 
 
 func test1(script: VirtualScript):
-	script.register_function("my_func", VirtualScript.Function.Type.CUSTOM, ["a", "b"], VirtualScript.Code.new(script).create_inline("return a + b"))
+	script.register_function("my_func", VirtualScript.FunctionDefinition.Type.CUSTOM, ["a", "b"], VirtualScript.Code.new(script).create_inline("return a + b", script).root)
+	#script.register_function("my_func", VirtualScript.FunctionDefinition.Type.CUSTOM, ["a", "b"], VirtualScript.Code.new(script).create_inline("return a + b", script))
 	#script.register_function("my_func", VirtualScript.Function.Type.CUSTOM, ["a"], VirtualScript.Code.new(script).create_inline("return a"))
 	
 	script.add_line("var x: int")
@@ -48,18 +50,13 @@ func test1(script: VirtualScript):
 
 
 func test2(script: VirtualScript):
+	pass
 	
-	script.add_line("var x: int")
-	script.add_line("x = 1")
 
-	script.add_line("if true:")
-	#script.add_line("pass")
-	script.add_line("if false:")
-	script.add_line("x= 100")
-	script.close_block()
-	script.add_line("else:")
-	script.add_line("x = -100")
-	script.close_block()
-	script.close_block()
-	script.add_line("else:")
-	script.add_line("x = -1")
+func node_test2(script: VirtualScript):
+	$Sprite2D.show()
+	VirtualScriptManager.attach_script(script, $Sprite2D)
+
+	script.register_function("_process", VirtualScript.FunctionDefinition.Type.CUSTOM, ["delta"], VirtualScript.Code.new(script).create_inline("position = position + Vector2(delta * 10, 0)", script).root)
+	#script.add_line("position= position - Vector2(200, 200)")
+
